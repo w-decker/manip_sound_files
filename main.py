576f0689d5882ab044ac08724cf570e427fd0bf4
Your Name <deckerwill7@gmail.com>
2023-09-28 11:54:59 -0500

    full scale

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # imports 
 from pydub import AudioSegment
 import pandas as pd
+import numpy as np
 
 # load in list of files 
 d = pd.read_csv('files2read.csv')
@@ -10,8 +11,20 @@ d.describe
 d.columns
 
 # test with two sounds
+sounds1 = AudioSegment.from_wav('sounds/' + d['sounds'][1] + '.wav')
+sounds2 = AudioSegment.from_wav('sounds/' + d['sounds'][2] + '.wav')
+sounds = sounds1 + sounds2
+sounds.export("cat_sound_test.wav", format='wav')
+
+# fullscale
 sounds = []
-for i in range(0):
-    sounds[i] = AudioSegment.from_wav('sounds/' + d['sounds'][i] + '.wav')
-    sounds[i + 1] = AudioSegment.from_wav('sounds/' + d['sounds'][i+1] + '.wav')
-    sounds.append(sounds[i] + sounds[i+1])
+for i in range(len(d['sounds'])):
+    sound = AudioSegment.from_wav('sounds/' + d['sounds'][i] + '.wav')
+    sounds.append(sound)
+
+for i in range(1, len(sounds)):
+    sound2write = sounds[0] + sounds[i]
+    
+sound2write.export('grandsound.wav', format="wav")
+
+
